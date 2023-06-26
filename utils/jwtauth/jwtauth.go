ac749bc4a29328225ff2b02fd40173da9ed6444d
Sumit Thakur <sumit.thakur@brank.as>
2023-06-26 09:53:48 +0530

    Update: Update the jwtauth code

--- utils/jwtauth/jwtauth.go
+++ utils/jwtauth/jwtauth.go
@@ -1,7 +1,7 @@
 package jwtauth
 
 import (
-	"crypto/rsa"
+	"fmt"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -12,23 +12,18 @@ const (
 	SUB = "sub"
 )
 
+var secretKey = []byte("/7fjzEeQOd762BnRCDSL3QDCzhlqq4gSAnlnAdYSPRY=")
+
 // Generate the Access token
-func GenerateToken(key *rsa.PublicKey, username string, uuid string) (string, error) {
+func GenerateToken(username string, uuid string) (string, error) {
 	// Create the token
 	token := jwt.New(jwt.SigningMethodRS512)
 	claims := make(jwt.MapClaims)
 	claims["username"] = username
-	claims["exp"] = time.Now().Add(time.Minute * 1).Unix()
-	claims["sub"] = uuid
+	claims[EXP] = time.Now().Add(time.Minute * 1).Unix()
+	claims[SUB] = uuid
 	token.Claims = claims
-
-	// Sign the token with the RSA private key
-	tokenString, err := token.SignedString(key)
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, nil
+	return token.SignedString(secretKey)
 }
 
 // Logout the user
@@ -51,4 +46,21 @@ func ParseToken(myToken string, key []byte) bool {
 	} else {
 		return false
 	}
+}
+
+func VerifyToken(tokenString string) (*jwt.Token, error) {
+	// Parse the token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validate the signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
 }
